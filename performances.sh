4c52fd1cc6913cd03c06223a96db7c6b40188345
Brulans4 <nicolas.heymans@hotmail.com>
2025-11-14 10:17:45 +0100

    feat (preformance.sh): creat performance scripte

---
+++ performances.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+make
+
+#philosophes
+PHILOSOPHES_CSV=philosophes.csv
+PHILOSOPHES_EXEC=./philosophes
+echo "Threads,try,time" > $PHILOSOPHES_CSV
+
+for i in 2 4 8 16 32
+do
+    for j in {1..5}
+    do
+        echo "nbThreads : $i, try : $j"
+        TIME_RES=$(/usr/bin/time -f "%e" $PHILOSOPHES_EXEC $i 2>&1)
+        echo "$i,$j,$TIME_RES" >> $PHILOSOPHES_CSV
+    done
+done
+
+#consumer
+
+
+#rider writer
+
+
+
+
+
+make clean
+
+exit 
